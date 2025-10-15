3579e2fe2d0e6eb62d512013f3bf0f9aae143240
Lucas Pelanda <lucaspelanda@MacBook-Pro-de-Lucas-2.local>
2025-10-15 18:40:46 -0300

    função hash resto da divisão v3

---
+++ PJBL_HASH/RestoDivisao/Main.java
@@ -0,0 +1,90 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        long seed = 32;
+        Random random = new Random(seed);
+        Resultado[] resultados = new Resultado[27];
+        int indiceResultado = 0;
+
+        int[] tamanhosConjuntos = {100000, 1000000, 10000000};
+
+        int[] vetor1 = new int[tamanhosConjuntos[0]];
+        int[] vetor2 = new int[tamanhosConjuntos[1]];
+        int[] vetor3 = new int[tamanhosConjuntos[2]];
+        int[][] vetores = {vetor1, vetor2, vetor3};
+
+        for (int[] vetor : vetores) {
+            preencherVetor(vetor, random);
+        }
+
+        int[] tamanhosTabelas = {1000, 10000, 100000};
+
+        // Teste para cada tamanho de tabela e conjunto
+        for (int tamanhoTabela : tamanhosTabelas) {
+
+            for (int j = 0; j < vetores.length; j++) {
+                RDivisao tabela = new RDivisao(tamanhoTabela);
+
+                // Inserção
+                tabela.setInicioInsersao(System.currentTimeMillis());
+                for (int chave : vetores[j]) {
+                    tabela.inserir(chave);
+                }
+                tabela.setFimInsersao(System.currentTimeMillis());
+
+                // Busca
+                tabela.setInicioBusca(System.currentTimeMillis());
+                for (int chave : vetores[j]) {
+                    tabela.buscar(chave);
+                }
+                tabela.setFimBusca(System.currentTimeMillis());
+
+                int[] medicoesLista = tabela.medirListas();
+                int primeiraLista = medicoesLista[0];
+                int segundaLista = medicoesLista[1];
+                int terceiraLista = medicoesLista[2];
+
+                int[] medicoesGaps = tabela.medirGaps();
+                int maiorGap = medicoesGaps[0];
+                int menorGap = medicoesGaps[1];
+                int mediaGaps = medicoesGaps[2];
+
+                System.out.println(
+                        tabela.getTipo() + " -> " + tabela.getTamanho() + " Posições -> " + tamanhosConjuntos[j] + " Elementos" +
+                                " -> Colisões: " + tabela.getColisoes() +
+                                " -> Tempo de Inserção: " + tabela.getTempoInsersao() + "ms" +
+                                " -> Tempo de Busca: " + tabela.getTempoBusca() + "ms" +
+                                " -> Maior Lista: " + primeiraLista +
+                                " -> Segunda Lista: " + segundaLista +
+                                " -> Terceira Lista: " + terceiraLista +
+                                " -> Maior Gap: " + maiorGap +
+                                " -> Menor Gap: " + menorGap +
+                                " -> Média Gaps: " + mediaGaps);
+
+                resultados[indiceResultado] = new Resultado(
+                        tabela.getTipo(), tabela.getTamanho(), tamanhosConjuntos[j],
+                        tabela.getTempoInsersao(), tabela.getTempoBusca(), tabela.getColisoes(),
+                        primeiraLista, segundaLista, terceiraLista,
+                        maiorGap, menorGap, mediaGaps
+                );
+
+                indiceResultado++;
+            }
+        }
+    }
+
+    // Gera números aleatórios únicos em cada vetor
+    public static void preencherVetor(int[] vetor, Random random) {
+        Set<Integer> usados = new HashSet<>();
+        int i = 0;
+
+        while (i < vetor.length) {
+            int num = random.nextInt(100_000_000); // espaço grande = menos colisões
+            if (usados.add(num)) { // adiciona só se for novo
+                vetor[i++] = num;
+            }
+        }
+    }
+}
---
+++ PJBL_HASH/RestoDivisao/RDivisao.java
@@ -0,0 +1,122 @@
+public class RDivisao extends TabelaHash {
+    private ListaEncadeada[] tabela;
+
+    public RDivisao(int tamanho) {
+        super(tamanho, "Resto de Divisão");
+        tabela = new ListaEncadeada[tamanho];
+        for (int i = 0; i < tamanho; i++) {
+            tabela[i] = new ListaEncadeada();
+        }
+    }
+
+    @Override
+    public int hash(int chave) {
+        if (chave < 0) chave = -chave;
+        return chave % tamanho;
+    }
+
+    @Override
+    public void inserir(int chave) {
+        int posicao = hash(chave);
+        ListaEncadeada lista = tabela[posicao];
+        Node novo = new Node(chave);
+
+        Node atual = lista.getPrimeiro();
+        Node anterior = null;
+
+        while (atual != null && atual.getValor() < chave) {
+            anterior = atual;
+            atual = atual.getProximo();
+            colisoes++;
+        }
+
+        if (anterior == null) {
+            // Inserir no início
+            novo.setProximo(lista.getPrimeiro());
+            lista.setPrimeiro(novo);
+        } else {
+            // Inserir no meio ou fim
+            novo.setProximo(atual);
+            anterior.setProximo(novo);
+        }
+
+        tabela[posicao].setTamanhoLista(tabela[posicao].getTamanhoLista() + 1);
+    }
+
+    @Override
+    public boolean buscar(int chave) {
+        int pos = hash(chave);
+        return tabela[pos].buscar(chave);
+    }
+
+    // MEDICAO DOS TAMANHOS DAS LISTAS
+    public int[] medirListas() {
+        int primeiro = 0;
+        int segundo = 0;
+        int terceiro = 0;
+
+        for (ListaEncadeada lista : tabela) {
+            int tamanho = lista.getTamanhoLista();
+
+            if (tamanho > primeiro) {
+                terceiro = segundo;
+                segundo = primeiro;
+                primeiro = tamanho;
+            } else if (tamanho > segundo) {
+                terceiro = segundo;
+                segundo = tamanho;
+            } else if (tamanho > terceiro) {
+                terceiro = tamanho;
+            }
+        }
+
+        return new int[] {primeiro, segundo, terceiro};
+    }
+
+    // MEDICAO DOS GAPS
+    public int[] medirGaps() {
+        int maior = 0;
+        int menor = tamanho;
+        int soma = 0;
+        int qtdGaps = 0;
+        int contador = 0;
+
+        for (ListaEncadeada lista : tabela) {
+            if (lista.getPrimeiro() == null) {
+                contador++;
+            } else if (contador > 0) {
+                qtdGaps++;
+                soma += contador;
+                if (contador > maior){
+                    maior = contador;
+                }
+                if (contador < menor){
+                    menor = contador;
+                }
+                contador = 0;
+            }
+        }
+
+        if (contador > 0) {
+            qtdGaps++;
+            soma += contador;
+            if (contador > maior) maior = contador;
+            if (contador < menor) menor = contador;
+        }
+
+        int media = 0;
+        if(qtdGaps > 0){
+            media = soma / qtdGaps;
+        }
+
+        if (menor == tamanho) menor = 0;
+
+        return new int[]{maior, menor, media};
+    }
+
+    public void imprimir(){
+        for(ListaEncadeada lista: tabela){
+            lista.imprimir();
+        }
+    }
+}
